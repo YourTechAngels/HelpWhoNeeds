011fd3421167cc48ebc95ee7eb7f1eff8c916dd2
Kusum <Kusum-Thapa@users.noreply.github.com>
2020-12-30 22:42:47 +0000

    Implemented ability to accept and reject task

--- frontend/src/components/volunteerTask/VolunteerSearchTask.js
+++ frontend/src/components/volunteerTask/VolunteerSearchTask.js
@@ -18,29 +18,137 @@ const options = {
 };
 
 const useStyles = makeStyles({
-    h1: {
+    h5: {
         color: "#4C4B51",
         textAlign: "center",
     },
-    cellDiv: {
-        width: "50%",
-        backgroundColor: "white",
-    }
+   
 });
-
+const intialTasks = [
+    {
+        id: 1,
+        lastName: "Snow",
+        firstName: "Jon",
+        taskType: "Shopping",
+        taskSummary: "I need help with Shopping from Tesco",
+        date: "2020-12-28",
+        distance: 1,
+        volId: 1,
+    },
+    {
+        id: 2,
+        lastName: "Lannister",
+        firstName: "Cersei",          
+        taskType: "Dog Walking",
+        taskSummary: "I need help with dog walkingevery morning and afternoon",
+        date: "2021-01-20",
+        distance: 1,
+        volId: null,
+    },
+    {
+        id: 3,
+        lastName: "Lannister",
+        firstName: "Jaime",
+        taskType: "Shopping",
+        taskSummary: "I need help with Shopping from Asda",
+        date: "2021-01-28",
+        distance: 2.3,
+        volId: null,
+    },
+    {
+        id: 4,
+        lastName: "Stark",
+        firstName: "Arya",
+        taskType: "Pharmacy",
+        taskSummary: "I need help with picking up my prescription from local gp",
+        date: "2021-03-20",
+        distance: 0.8,
+        volId: null,
+    },
+    {
+        id: 5,
+        lastName: "Targaryen",
+        firstName: "Daenerys",
+        taskType: "Hospital",
+        taskSummary: "I need help to drop off and pick up from my hospital",
+        date: "2021-01-10",
+        distance: 2,
+        volId: null,
+    },
+    {
+        id: 6,
+        lastName: "Melisandre",
+        firstName: null,
+        taskType: "Phone Call",
+        taskSummary: "I need someone to give me a call to have a chat",
+        date: "2021-01-20",
+        distance: 5,
+        volId: null,
+    },
+    {
+        id: 7,
+        lastName: "Clifford",
+        firstName: "Ferrara",
+        taskType: "Shopping",
+        taskSummary: "I need help with Shopping from Tesco",
+        date: "2021-01-20",
+        distance: 4,
+        volId: null,
+    },
+    {
+        id: 8,
+        lastName: "Frances",
+        firstName: "Rossini",
+        taskType: "Other",
+        taskSummary: "I need help with mowing my garden",
+        date: "2021-01-20",
+        distance: 2,
+        volId: null,
+    },
+    {
+        id: 9,
+        lastName: "Roxie",
+        firstName: "Harvey",
+        taskType: "Shopping",
+        taskSummary: "I need help with Shopping from Sainsbuyrys",
+        date: "2021-01-20",
+        distance: 3,
+        volId: null,
+    },
+    {
+        id: 10,
+        lastName: "Indra",
+        firstName: "Thapa",
+        taskType: "Medical",
+        taskSummary: "I need help with picking up my prescription",
+        date: "2021-01-20",
+        distance: 3,
+        volId: null,
+    },
+];
 export default function VolunteerSearchTask() {
     const classes = useStyles();
     const theme = createMuiTheme({
         overrides: {
             MuiDataTable: {
-                responsiveScroll: {
-                    maxHeight: '300px'
+                    root:{
+                        width: "min-content"
+                    },
+                    responsiveScroll: {
+                    maxHeight: '300px' //not working
+                }
+            },
+            MUIDataTableBodyCell: {
+                root: {
+                backgroundColor: "#FFF",
+                width: "100px"
                 }
             }
         }
     });
 
-  const [pendingTasks, setPendingTasks] = useState([
+  const [pendingTasks, setPendingTasks] = useState(intialTasks);
+  /*useState([
         {
             id: 1,
             lastName: "Snow",
@@ -49,7 +157,7 @@ export default function VolunteerSearchTask() {
             taskSummary: "I need help with Shopping from Tesco",
             date: "2020-12-28",
             distance: 1,
-            volId: null,
+            volId: 1,
         },
         {
             id: 2,
@@ -141,12 +249,12 @@ export default function VolunteerSearchTask() {
             distance: 3,
             volId: null,
         },
-    ]);
+    ]);*/
     const [showDialog, setShowDialog] = React.useState(false);
     const [dialogData, setDialogData] = React.useState(null);
 
     const handleClickOpen = (e, dialogData) => {
-        //setShowDialog(true)
+        setShowDialog(true)
         setDialogData(dialogData)
     }
 
@@ -159,6 +267,11 @@ export default function VolunteerSearchTask() {
         {
             name: "id",
             label: "ID",
+            options: { display: false, sort: false, filter: false },
+        },
+        {
+            name: "volId",
+            label: "volunteer ID",
             options: { display: false, sort: false, filter: false },
         },
         {
@@ -219,9 +332,6 @@ export default function VolunteerSearchTask() {
                 sort: false,
                 customBodyRender: (value, tableMeta, updateValue) => {
                     return (
-                        <div className={classes.cellDiv} >
-
-
                             <Button
                                 variant="contained"
                                 color="primary"
@@ -232,7 +342,6 @@ export default function VolunteerSearchTask() {
                             >
                                 View
                         </Button>
-                        </div>
                     );
                 },
             },
@@ -245,25 +354,64 @@ export default function VolunteerSearchTask() {
             options: {
                 sort: false,
                 filter: false,
-                customBodyRender: (value, tableMeta, updateValue) => {
+                customBodyRender: (value, tableMeta) => {
                     return (
                         <Button
                             variant="contained"
                             color="primary"
                             size="small"
-                            style={{ marginLeft: 16, backgroundColor: "green" }}
+                            disabled = {tableMeta.rowData[1] === null ? false : true}
+                            style={{ marginLeft: 16, backgroundColor: tableMeta.rowData[1] === null ? "green": "lightgrey" }}
                             className="button"
                             value={value}
                             onClick={() => {
-                               // console.log(value);
-                               // console.log(tableMeta.rowData);
-                                const updatedPendingTask = pendingTasks.filter(
+                                console.log(tableMeta.rowData[1] ); 
+                                setPendingTasks(
+                                    pendingTasks.map(task => 
+                                        task.id === value 
+                                        ? {...task, volId : 1} 
+                                        : task )
+                                )                                 
+                               /* const updatedPendingTask = pendingTasks.filter(
                                     (task) => task.id !== value
                                 );
-                                setPendingTasks(updatedPendingTask);
+                                setPendingTasks(updatedPendingTask);*/
                             }}
                         >
                             Accept
+                        </Button>
+                    );
+                },
+            },
+        },
+        {
+            name: "id",
+            label: "Cancel",
+
+            options: {
+                sort: false,
+                filter: false,
+                customBodyRender: (value, tableMeta) => {
+                    return (
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            size="small"
+                            disabled = {tableMeta.rowData[1] === null ? true : false}
+                            style={{ marginLeft: 16}}
+                            className="button"
+                            value={value}
+                            onClick={() => {
+                                console.log(tableMeta.rowData[1] ); 
+                                setPendingTasks(
+                                    pendingTasks.map(task => 
+                                        task.id === value 
+                                        ? {...task, volId : null} 
+                                        : task )
+                                )            
+                            }}
+                        >
+                            Reject
                         </Button>
                     );
                 },
@@ -273,9 +421,8 @@ export default function VolunteerSearchTask() {
 
     return (
         <React.Fragment>
-
-            <div style={{ height: "100%", width: "100%" }}>
-                <h1 className={classes.h1}>Search Task</h1>
+            <div style={{ height: "100%"}}>
+                <h4 className={classes.h5}>Search Task</h4>
                 <MuiThemeProvider theme={theme}>
                     <MUIDataTable
                         title={"Pending Task List"}
@@ -285,8 +432,19 @@ export default function VolunteerSearchTask() {
                         
                     />                   
                 </MuiThemeProvider>
+                <Button
+                                variant="contained"
+                                color="primary"
+                                size="small"
+                                style={{ marginLeft: 16 }}
+                                                              
+                                                 
+                            >
+                                Search Task
+                        </Button>
                 <TaskDialog open={showDialog} handleClose={handleClose} title="Task Summary" data={dialogData} />   
             </div>
+            
         </React.Fragment>
     );
 }
