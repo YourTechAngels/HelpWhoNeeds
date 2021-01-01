4ee0d2924b9a1e78d090ca9a5916fbdf4bad6e81
Nathalie Rud <nathalier@gmail.com>
2021-01-01 18:34:10 +0000

    add base Tasks model

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("task_type", "description", "dbs_needed", "start_time",
+                    "end_time", "min_duration", "status")
+
+admin.site.register(Task, TaskAdmin)
