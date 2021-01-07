24f2b0748236aa76e0dd99be477bea24b925cd4c
Nathalie Rud <nathalier@gmail.com>
2021-01-07 14:16:24 +0000

    add search_fields for admin

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -5,5 +5,6 @@ from .models import Task
 class TaskAdmin(admin.ModelAdmin):
     list_display = ("task_type", "description", "dbs_needed", "start_time",
                     "end_time", "min_duration", "status")
+    search_fields = ("task_type", "description")
 
 admin.site.register(Task, TaskAdmin)
