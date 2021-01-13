e17a89bad137d2ceca6f535fbff44426d7234763
Kusum <Kusum-Thapa@users.noreply.github.com>
2021-01-13 10:59:37 +0000

    Feat: Changed the datatypes and fieldnames to match the data from db

--- frontend/src/components/volunteerTask/TaskDetail.js
+++ frontend/src/components/volunteerTask/TaskDetail.js
@@ -8,27 +8,24 @@ import {
 import Grid from "@material-ui/core/Grid";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useTheme } from "@material-ui/core/styles";
-import { TextField } from '@material-ui/core';
+import { TextField } from "@material-ui/core";
 
-
-
-const useStyles = theme => ({
-    textFld: { width: '85%', height: 40, paddingLeft: 8 } , 
+const useStyles = (theme) => ({
+    textFld: { width: "85%", height: 40, paddingLeft: 8 },
     button: {
-      border: '4px',
-      color: "default",
-      fontWeight: 'bold',
-      marginLeft: 10,
-      marginTop: '10px',
+        border: "4px",
+        color: "default",
+        fontWeight: "bold",
+        marginLeft: 10,
+        marginTop: "10px",
     },
     form: {
-        display: 'flex',
-        flexDirection: 'column',
-        margin: 'auto',
-        width: 'fit-content',
-      },
-    
-    });
+        display: "flex",
+        flexDirection: "column",
+        margin: "auto",
+        width: "fit-content",
+    },
+});
 
 const TaskDialog = ({ open, handleClose, title, data }) => {
     const theme = useTheme();
@@ -44,28 +41,36 @@ const TaskDialog = ({ open, handleClose, title, data }) => {
             >
                 <DialogTitle>{title}</DialogTitle>
                 <DialogContent>
-                <form className={useStyles.form} noValidate>
+                    <form className={useStyles.form} noValidate>
                         {data && (
                             <Grid container spacing={3}>
-                                 {data.firstName} {data.lastName}
-                                <Grid item xs={12} sm={12} >
-                                <TextField id="taskType" type="string" label="Task" value={data.taskType}  variant="outlined"
-                                style = {useStyles.textFld}
-                                autoComplete="family-name" />
+                                {data.firstName} {data.lastName}
+                                <Grid item xs={12} sm={12}>
+                                    <TextField
+                                        id="taskType"
+                                        type="string"
+                                        label="Task"
+                                        value={data.taskType}
+                                        variant="outlined"
+                                        style={useStyles.textFld}
+                                        autoComplete="family-name"
+                                    />
                                 </Grid>
-                                <Grid item xs={12} sm={12} >
-                                <TextField id="taskSummary" type="string" label="Task Summary" value={data.taskSummary}  variant="outlined"
-                                multiline
-                                rows={6}
-                                style = {useStyles.textFld}
-                                autoComplete="family-name" />
+                                <Grid item xs={12} sm={12}>
+                                    <TextField
+                                        id="taskDetails"
+                                        type="string"
+                                        label="Task Summary"
+                                        value={data.taskDetails}
+                                        variant="outlined"
+                                        multiline
+                                        rows={6}
+                                        style={useStyles.textFld}
+                                        autoComplete="family-name"
+                                    />
                                 </Grid>
-                                <Grid item xs={12} sm={6} >
-                                    
-                                </Grid>
-                                <Grid item xs={12} sm={6} >
-                                    
-                                </Grid>
+                                <Grid item xs={12} sm={6}></Grid>
+                                <Grid item xs={12} sm={6}></Grid>
                             </Grid>
                         )}
                     </form>
