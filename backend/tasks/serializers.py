4ae8374d039f9b97f6471d2dd0ae4de4cdd03e52
Nathalie Rud <nathalier@gmail.com>
2021-01-18 14:23:49 +0000

    dbs_needed field name changed in tasks serializer

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import Task
 
+
 class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = ("id", "task_type", "description", "dbs_needed", "start_time",
+        fields = ("id", "task_type", "description", "dbs_required", "start_time",
                   "end_time", "status", "requestee", "volunteer")
         depth = 1
