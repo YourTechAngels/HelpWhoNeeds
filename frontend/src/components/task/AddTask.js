03194b5750bd5fbb8a5d5174d126346d95f59171
Nathalie Rud <nathalier@gmail.com>
2021-01-21 00:56:42 +0000

    prettify tasks from DB view

--- frontend/src/components/task/AddTask.js
+++ frontend/src/components/task/AddTask.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { useState } from "react"
 import Button from '@material-ui/core/Button'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import PetsIcon from '@material-ui/icons/Pets'
@@ -10,7 +9,9 @@ import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
 import LiveHelpIcon from '@material-ui/icons/LiveHelp'
 import NewTaskForm from './NewTaskForm'
 import TasksTable from './TaskTable'
-import initialTasks from "./tasksDataOnly"
+import axios from "axios"
+import { useAuth } from "../../contexts/AuthContext";
+
 
 const useStyles =
     makeStyles(
@@ -40,127 +41,181 @@ const useStyles =
         })
 
 
+
 function AddTask() {
 
-    const [taskList, setTaskList] = useState(initialTasks())
-    const [nextId, setNextId] = useState(11)
-
-    const addTask = newTask => {
-        newTask.id = nextId
-        setNextId(nextId + 1)
-        newTask.status = "Open"
-        const updatedTaskList = [...taskList, newTask]
-        setTaskList(updatedTaskList)
+    const readableTaskTypes = {
+        "GRO": "Shopping",
+        "PHA": "Pharmacy",
+        "DOG": "Dog Walking",
+        "HOS": "Hospital",
+        "CHAT": "Chat",
+        "ANY": "Other",
     }
 
-    const updateTask = (updTask, id) => {
-        if (id < 0) { // should not happen but in case
-            console.log("ERROR: task to be updated does not exists")
-            return }
-        if (updTask.end > new Date())  // expected to be so but just in case
-            updTask.status = "Open"
-        updTask.id = id
-        let updatedTaskList = taskList.filter(task => task.id !== id)
-        updatedTaskList = [...updatedTaskList, updTask]
-        setTaskList(updatedTaskList)
+    const readableStatus = {
+        "OP": "Open",
+        "EXP": "Expired",
+        "AS": "Assigned",
+        "CL": "Canceled",
+        "DN": "Completed",}
+
+const { currentUser } = useAuth()
+const userUID = "WNVuNlpmfs" // currentUser.uid
+const [taskList, setTaskList] = useState([])
+
+useEffect(() => {
+    const options = {
+        method: 'GET',
+        url: "/api/requestee/tasks/",
+        timeout: 8000,
+        params: {
+            requid: userUID,
+        },
     }
-
-    const taskDefaults = {
-        taskDetails: "",
-        startDate: null,
-        startTime: "08:00",
-        endDate: null,
-        endTime: "20:00"
-    }
-
-    const [showAddDialog, setShowAddDialog] = React.useState(false);
-    const [taskType, setTaskType] = React.useState(null);
-    const [newTaskDefaults, setNewTaskDefaults] = React.useState(taskDefaults)
-    const [updTaskId, setUpdTaskId] = React.useState(-1)
-
-    const handleClickOpen = (e, taskType) => {
-        setTaskType(taskType)
-        setShowAddDialog(true)
-    }
-
-    const handleClose = () => {
-        setTaskType(null)
-        setShowAddDialog(false)
-        setNewTaskDefaults(taskDefaults)
-        setUpdTaskId(-1)
-    }
-
-    const handleCopy = id => {
-        const taskToCopy = taskList.find(task => task.id === id)
-        setTaskType(taskToCopy.taskType)
-        setNewTaskDefaults({
-            ...newTaskDefaults,
-            taskDetails: taskToCopy.taskDetails,
+    axios(options)
+        .then((response) => {
+            console.log(response.data)
+            const taskData = response.data.map(task => {
+                return (
+                    {
+                        id: `${task.id}`,
+                        taskType: readableTaskTypes[`${task.task_type.task_type}`],
+                        taskDetails: `${task.description}`,
+                        start: `${task.start_time}`,
+                        end: (`${task.end_time}`),
+                        status: readableStatus[`${task.status}`]
+                    })
+            })
+            setTaskList(taskData)
         })
-        setShowAddDialog(true)
-    }
-
-    const handleEdit = id => {
-        const taskToEdit = taskList.find(task => task.id === id)
-        setTaskType(taskToEdit.taskType)
-        setNewTaskDefaults({
-            ...newTaskDefaults,
-            taskDetails: taskToEdit.taskDetails,
+        .catch(error => {
+            console.log("error")
+            console.log(error.request);
+            console.log(error.config);
+            console.log(error.message);
         })
-        setUpdTaskId(id)
-        setShowAddDialog(true)
+}, [])
+
+const [nextId, setNextId] = useState(11)
+
+const addTask = newTask => {
+    newTask.id = nextId
+    setNextId(nextId + 1)
+    newTask.status = "Open"
+    const updatedTaskList = [...taskList, newTask]
+    setTaskList(updatedTaskList)
+}
+
+const updateTask = (updTask, id) => {
+    if (id < 0) { // should not happen but in case
+        console.log("ERROR: task to be updated does not exists")
+        return
     }
+    if (updTask.end > new Date())  // expected to be so but just in case
+        updTask.status = "Open"
+    updTask.id = id
+    let updatedTaskList = taskList.filter(task => task.id !== id)
+    updatedTaskList = [...updatedTaskList, updTask]
+    setTaskList(updatedTaskList)
+}
 
-    const handleRemove = id => {
-        const copyTaskList = [...taskList]
-        let taskToCancel = copyTaskList.find(task => task.id === id)
-        taskToCancel.status = "Cancelled"
-        setTaskList(copyTaskList)
-    }
+const taskDefaults = {
+    taskDetails: "",
+    startDate: null,
+    startTime: "08:00",
+    endDate: null,
+    endTime: "20:00"
+}
 
-    const classes = useStyles();
+const [showAddDialog, setShowAddDialog] = React.useState(false);
+const [taskType, setTaskType] = React.useState(null);
+const [newTaskDefaults, setNewTaskDefaults] = React.useState(taskDefaults)
+const [updTaskId, setUpdTaskId] = React.useState(-1)
 
-    return <div className="centered">
+const handleClickOpen = (e, taskType) => {
+    setTaskType(taskType)
+    setShowAddDialog(true)
+}
 
-        <h1>I need help with...</h1>
+const handleClose = () => {
+    setTaskType(null)
+    setShowAddDialog(false)
+    setNewTaskDefaults(taskDefaults)
+    setUpdTaskId(-1)
+}
 
-        <Button classes={{ root: classes.largeButton, label: classes.label }}
-            onClick={(e) => handleClickOpen(e, "Shopping")}>
-            <ShoppingCartIcon className={classes.largeIcon} />
+const handleCopy = id => {
+    const taskToCopy = taskList.find(task => task.id === id)
+    setTaskType(taskToCopy.taskType)
+    setNewTaskDefaults({
+        ...newTaskDefaults,
+        taskDetails: taskToCopy.taskDetails,
+    })
+    setShowAddDialog(true)
+}
+
+const handleEdit = id => {
+    const taskToEdit = taskList.find(task => task.id === id)
+    setTaskType(taskToEdit.taskType)
+    setNewTaskDefaults({
+        ...newTaskDefaults,
+        taskDetails: taskToEdit.taskDetails,
+    })
+    setUpdTaskId(id)
+    setShowAddDialog(true)
+}
+
+const handleRemove = id => {
+    const copyTaskList = [...taskList]
+    let taskToCancel = copyTaskList.find(task => task.id === id)
+    taskToCancel.status = "Cancelled"
+    setTaskList(copyTaskList)
+}
+
+const classes = useStyles();
+
+return <div className="centered">
+
+    <h1>I need help with...</h1>
+
+    <Button classes={{ root: classes.largeButton, label: classes.label }}
+        onClick={(e) => handleClickOpen(e, "Shopping")}>
+        <ShoppingCartIcon className={classes.largeIcon} />
             Shopping
         </Button>
-        <Button classes={{ root: classes.largeButton, label: classes.label }}
-            onClick={(e) => handleClickOpen(e, "Pharmacy")}>
-            <LocalPharmacyIcon className={classes.largeIcon} />
+    <Button classes={{ root: classes.largeButton, label: classes.label }}
+        onClick={(e) => handleClickOpen(e, "Pharmacy")}>
+        <LocalPharmacyIcon className={classes.largeIcon} />
             Pharmacy
         </Button>
-        <Button classes={{ root: classes.largeButton, label: classes.label }}
-            onClick={(e) => handleClickOpen(e, "Dog Walking")}>
-            <PetsIcon className={classes.largeIcon} />
+    <Button classes={{ root: classes.largeButton, label: classes.label }}
+        onClick={(e) => handleClickOpen(e, "Dog Walking")}>
+        <PetsIcon className={classes.largeIcon} />
             Dog Walk
         </Button>
-        <Button classes={{ root: classes.largeButton, label: classes.label }}
-            onClick={(e) => handleClickOpen(e, "Hospital")}>
-            <LocalHospitalIcon className={classes.largeIcon} />
+    <Button classes={{ root: classes.largeButton, label: classes.label }}
+        onClick={(e) => handleClickOpen(e, "Hospital")}>
+        <LocalHospitalIcon className={classes.largeIcon} />
             Hospital
         </Button>
-        <Button classes={{ root: classes.largeButton, label: classes.label }}
-            onClick={(e) => handleClickOpen(e, "Chat")}>
-            <PhoneIcon className={classes.largeIcon} />
+    <Button classes={{ root: classes.largeButton, label: classes.label }}
+        onClick={(e) => handleClickOpen(e, "Chat")}>
+        <PhoneIcon className={classes.largeIcon} />
             Chat
         </Button>
-        <Button classes={{ root: classes.largeButton, label: classes.label }}
-            onClick={(e) => handleClickOpen(e, "Other")}>
-            <LiveHelpIcon className={classes.largeIcon} />
+    <Button classes={{ root: classes.largeButton, label: classes.label }}
+        onClick={(e) => handleClickOpen(e, "Other")}>
+        <LiveHelpIcon className={classes.largeIcon} />
             Other
         </Button>
 
-        <NewTaskForm open={showAddDialog} handleClose={handleClose} taskType={taskType}
-            addTask={addTask} defaultValues={newTaskDefaults} updateTask={updateTask} updTaskId={updTaskId} />
+    <NewTaskForm open={showAddDialog} handleClose={handleClose} taskType={taskType}
+        addTask={addTask} defaultValues={newTaskDefaults} updateTask={updateTask} updTaskId={updTaskId} />
 
-        <TasksTable taskList={taskList} handleCopy={handleCopy}
-            handleEdit={handleEdit} handleRemove={handleRemove} />
-    </div>
+    <TasksTable taskList={taskList} handleCopy={handleCopy}
+        handleEdit={handleEdit} handleRemove={handleRemove} />
+</div>
 }
 
 export default AddTask
--- frontend/src/components/task/TaskTable.js
+++ frontend/src/components/task/TaskTable.js
@@ -1,284 +1,262 @@
-import React, { useEffect } from "react"
+import React from "react"
 import moment from "moment";
 import MUIDataTable from "mui-datatables"
 import Button from "@material-ui/core/Button"
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import axios from "axios"
 
 
-export default function TaskTable({ taskList, handleCopy, handleEdit, handleRemove, setTaskList }) {
-
-    useEffect(() => {
-        const options = {
-            method: 'GET',
-            url: "/api/requestee/tasks/?requid=jysXLRQKGr",
-            timeout: 8000,
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }
-        axios(options)
-            .then((response) => {
-                console.log(response.data)
-                const taskData = response.data.map(task => {
-                    return (
-                        {
-                            id: `${task.id}`,
-                            taskType: `${task.task_type.task_type}`,
-                            taskDetails: `${task.description}`,
-                            start: `${task.start_time}`,
-                            end: (`${task.end_time}`),
-                            status: `${task.status}`
-                        })
-                })
-                setTaskList(taskData)
-            })
-            .catch(error => {
-                console.log("error")
-                console.log(error.request);
-                console.log(error.config);
-                console.log(error.message);
-            })
-    }, [])
+export default function TaskTable({ taskList, handleCopy, handleEdit, handleRemove }) {
 
 
-const theme = () => createMuiTheme({
-    overrides: {
-        // MUIDataTableToolbar: {
-        //     root: {
-        //         // width: "min-content",
-        //         // margin: "20px 0px 0px 0px",
-        //         backgroundColor: '#AAF',
-        //     },
-        // },
-        MuiPaper: {
-            root: {
-                // width: "min-content",
-                margin: "20px 0px 0px 0px",
-                // backgroundColor: '#AAF',
+    const theme = () => createMuiTheme({
+        overrides: {
+            // MUIDataTableToolbar: {
+            //     root: {
+            //         // width: "min-content",
+            //         // margin: "20px 0px 0px 0px",
+            //         backgroundColor: '#AAF',
+            //     },
+            // },
+            MuiPaper: {
+                root: {
+                    // width: "min-content",
+                    margin: "20px 0px 0px 0px",
+                    // backgroundColor: '#AAF',
+                },
             },
-        },
-        MuiTableCell: {
-            root: {
-                //  maxWidth: "120px",
-                padding: "5px 5px 5px 15px"
+            MuiTableCell: {
+                root: {
+                    //  maxWidth: "120px",
+                    padding: "5px 5px 5px 15px"
+                },
             },
+            MUIDataTableToolbar: {
+                root: {
+                    background: '#AAF',
+                }
+            },
+            MUIDataTableHeadCell: {
+                root: {
+                    //   height: 56,
+                    //   fontSize: 12,
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                    position: "relative"
+                }
+            },
+            MUIDataTableBodyCell: {
+                root: {
+                    //   height: 20,
+                    //   fontFamily: "Oxygen",
+                    //   fontSize: 13,
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                    position: "relative",
+                    //   maxWidth: 0
+                }
+            },
+            // MUIDataTablePagination: {
+            //     root: {
+            //         display: "flex",
+            //         justifyContent: "center",
+            //         alignItems: "center",
+            //         textAlign: "center"
+            //     },
+            //     caption: {
+            //         fontSize: 16
+            //     }
+            // }
         },
-        MUIDataTableToolbar: {
-            root: {
-                background: '#AAF',
-            }
-        },
-        MUIDataTableHeadCell: {
-            root: {
-                //   height: 56,
-                //   fontSize: 12,
-                overflow: "hidden",
-                whiteSpace: "nowrap",
-                textOverflow: "ellipsis",
-                position: "relative"
-            }
-        },
-        MUIDataTableBodyCell: {
-            root: {
-                //   height: 20,
-                //   fontFamily: "Oxygen",
-                //   fontSize: 13,
-                overflow: "hidden",
-                whiteSpace: "nowrap",
-                textOverflow: "ellipsis",
-                position: "relative",
-                //   maxWidth: 0
-            }
-        },
-        // MUIDataTablePagination: {
-        //     root: {
-        //         display: "flex",
-        //         justifyContent: "center",
-        //         alignItems: "center",
-        //         textAlign: "center"
-        //     },
-        //     caption: {
-        //         fontSize: 16
-        //     }
-        // }
-    },
-});
+    });
 
-const options = {
-    selectableRows: "none",
-    print: false,
-    download: false,
-    rowsPerPage: 20,
-    rowsPerPageOptions: [5, 10, 20],
+    const options = {
+        selectableRows: "none",
+        print: false,
+        download: false,
+        rowsPerPage: 20,
+        rowsPerPageOptions: [5, 10, 20],
+        setRowProps: row => {
+            if (row[5] === "Assigned") {
+                return {
+                    style: { background: "palegreen" }
+                }
+            }
+            if (row[5] === "Open") {
+                return {
+                    style: { background: "peachpuff" }
+                }
+            }
+        },
     // resizableColumns: true,
     sortOrder: {
-        name: "start",
-        direction: "desc",
-    },
-};
-
-const columns = [
-    {
-        name: "id",
-        label: "ID",
-        width: "5%",
-        options: { display: false, sort: true, filter: false },
-    },
-    {
-        name: "taskType",
-        label: "Task Type",
-
-        options: {
-            filter: true,
-            sort: true,
-            width: "10%",
+            name: "start",
+            direction: "desc",
         },
-    },
-    {
-        name: "taskDetails",
-        label: " Task Detail",
+    };
 
-        options: {
-            display: true,
-            filter: false,
-            sort: false,
-            width: "15%",
+    const columns = [
+        {
+            name: "id",
+            label: "ID",
+            width: "5%",
+            options: { display: false, sort: true, filter: false },
+        },
+        {
+            name: "taskType",
+            label: "Task Type",
 
-            customBodyRender: (value) => {
-                const maxSymb = 30
-                return value.slice(0, maxSymb) + (value.length > maxSymb ? "..." : "")
+            options: {
+                filter: true,
+                sort: true,
+                width: "10%",
             },
         },
-    },
-    {
-        name: "start",
-        label: "Start Time",
-        options: {
-            filter: true,
-            sort: true,
-            width: "10%",
+        {
+            name: "taskDetails",
+            label: " Task Detail",
 
-            customBodyRender: (value) =>
-                moment(value).format('lll'),
-        },
-    },
-    {
-        name: "end",
-        label: "End Time",
-        options: {
-            filter: true,
-            sort: true,
-            width: "10%",
+            options: {
+                display: true,
+                filter: false,
+                sort: false,
+                width: "15%",
 
-            customBodyRender: (value) =>
-                moment(value).format('lll'),
+                customBodyRender: (value) => {
+                    const maxSymb = 30
+                    return value.slice(0, maxSymb) + (value.length > maxSymb ? "..." : "")
+                },
+            },
         },
-    },
-    {
-        name: "status",
-        label: "State",
-        options: {
-            filter: true,
-            sort: true,
-            width: "10%",
-        },
-    },
-    {
-        name: "id",
-        label: "Actions",
+        {
+            name: "start",
+            label: "Start Time",
+            options: {
+                filter: true,
+                sort: true,
+                width: "10%",
 
-        options: {
-            sort: false,
-            filter: false,
-            width: "40%",
-            // customBodyRender: (value, tableMeta, updateValue) => {
-            //     return (
-            //         <Button
-            //             variant="contained"
-            //             color="primary"
-            //             size="small"
-            //             style={{ marginLeft: 16 }}
-            //             value={value}
-            //             onClick={(e) => handleClickOpen(e, value)}
-            //         >
-            //             View
-            //         </Button>
-            //     );
-            // },
-            customBodyRender: (value, tableMeta) => {
-                return (
-                    <div>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            size="small"
-                            // disabled={tableMeta.rowData[1] === null ? false : true}
-                            // style={{
-                            //     marginLeft: 16,
-                            //     backgroundColor:
-                            //         tableMeta.rowData[1] === null ? "green" : "lightgrey",
-                            // }}
-                            className="button"
-                            // value={value}
-                            onClick={() => {
-                                console.log(tableMeta.rowData[0])
-                                handleCopy(tableMeta.rowData[0])
-                            }}
-                        >
-                            Copy
+                customBodyRender: (value) =>
+                    moment(value).format('lll'),
+            },
+        },
+        {
+            name: "end",
+            label: "End Time",
+            options: {
+                filter: true,
+                sort: true,
+                width: "10%",
+
+                customBodyRender: (value) =>
+                    moment(value).format('lll'),
+            },
+        },
+        {
+            name: "status",
+            label: "State",
+            options: {
+                filter: true,
+                sort: true,
+                width: "10%",
+            },
+        },
+        {
+            name: "id",
+            label: "Actions",
+
+            options: {
+                sort: false,
+                filter: false,
+                width: "40%",
+                // customBodyRender: (value, tableMeta, updateValue) => {
+                //     return (
+                //         <Button
+                //             variant="contained"
+                //             color="primary"
+                //             size="small"
+                //             style={{ marginLeft: 16 }}
+                //             value={value}
+                //             onClick={(e) => handleClickOpen(e, value)}
+                //         >
+                //             View
+                //         </Button>
+                //     );
+                // },
+                customBodyRender: (value, tableMeta) => {
+                    return (
+                        <div>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                size="small"
+                                // disabled={tableMeta.rowData[1] === null ? false : true}
+                                // style={{
+                                //     marginLeft: 16,
+                                //     backgroundColor:
+                                //         tableMeta.rowData[1] === null ? "green" : "lightgrey",
+                                // }}
+                                className="button"
+                                // value={value}
+                                onClick={() => {
+                                    console.log(tableMeta.rowData[0])
+                                    handleCopy(tableMeta.rowData[0])
+                                }}
+                            >
+                                Copy
                         </Button>
-                        {["Open", "Expired"].includes(tableMeta.rowData[5]) ?
-                            <Button
-                                variant="contained"
-                                color="default"
-                                size="small"
-                                className="button"
-                                style={{
-                                    marginLeft: 10
-                                }}
-                                onClick={() => {
-                                    console.log(tableMeta.rowData[0])
-                                    handleEdit(tableMeta.rowData[0])
-                                }}
-                            >
-                                Edit
+                            {["Open", "Expired"].includes(tableMeta.rowData[5]) ?
+                                <Button
+                                    variant="contained"
+                                    color="default"
+                                    size="small"
+                                    className="button"
+                                    style={{
+                                        marginLeft: 10
+                                    }}
+                                    onClick={() => {
+                                        console.log(tableMeta.rowData[0])
+                                        handleEdit(tableMeta.rowData[0])
+                                    }}
+                                >
+                                    Edit
                     </Button>
-                            : null}
-                        {["Open"].includes(tableMeta.rowData[5]) ?
-                            <Button
-                                variant="contained"
-                                color="secondary"
-                                size="small"
-                                className="button"
-                                style={{
-                                    marginLeft: 10
-                                }}
-                                onClick={() => {
-                                    console.log(tableMeta.rowData[0])
-                                    handleRemove(tableMeta.rowData[0])
-                                }}
-                            >
-                                Cancel
+                                : null}
+                            {["Open"].includes(tableMeta.rowData[5]) ?
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    size="small"
+                                    className="button"
+                                    style={{
+                                        marginLeft: 10
+                                    }}
+                                    onClick={() => {
+                                        console.log(tableMeta.rowData[0])
+                                        handleRemove(tableMeta.rowData[0])
+                                    }}
+                                >
+                                    Cancel
                     </Button>
-                            : null}
-                    </div>
-                );
+                                : null}
+                        </div>
+                    );
+                },
             },
         },
-    },
-];
+    ];
 
-return (
-    <React.Fragment>
-        <MuiThemeProvider theme={theme()}>
-            <MUIDataTable
-                title="All Tasks"
-                data={taskList}
-                columns={columns}
-                options={options}
-            />
-        </MuiThemeProvider>
-    </React.Fragment>
-);
+    return (
+        <React.Fragment>
+            <MuiThemeProvider theme={theme()}>
+                <MUIDataTable
+                    title="All Tasks"
+                    data={taskList}
+                    columns={columns}
+                    options={options}
+                />
+            </MuiThemeProvider>
+        </React.Fragment>
+    );
 }
