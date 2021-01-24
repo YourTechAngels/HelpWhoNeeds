18be3342492a86160bd31a5b88a78eeb58c06b7e
sktrip <sktripurari@gmail.com>
2021-01-24 01:46:26 +0000

    updated views.py in accounts

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -8,12 +8,12 @@ class User(AbstractUser):
     username = models.CharField(max_length=150, blank=True, null=True, unique=True)
     email = models.EmailField(verbose_name='email', blank=True, null=True, unique=True)
     date_of_birth = models.DateField(blank=True, null=True)
-    phone_regex = RegexValidator(regex=r"^(?:0|\+?44)(?:\s|\-)?(?:\d(?:\s|\-)?){9,10}$",
-            message="Phone number must start with '0' or '+44', and contain only digits, spaces or dash symbols")
-    phone_number = models.CharField(validators=[phone_regex], max_length=20, blank=True)
+    # phone_regex = RegexValidator(regex=r"^(?:0|\+?44)(?:\s|\-)?(?:\d(?:\s|\-)?){9,10}$",
+    #         message="Phone number must start with '0' or '+44', and contain only digits, spaces or dash symbols")
+    phone_number = models.CharField(max_length=20, blank=True)
     post_code = models.CharField(max_length=150, blank=True)
     address_line_1 = models.CharField(max_length=150, blank=True)
-    address_line_2 = models.CharField(max_length=150, blank=True)
+    address_line_2 = models.CharField(max_length=150, blank=True, null=True)
     city = models.CharField(max_length=150, blank=True)
     county = models.CharField(max_length=150, blank=True)
     is_volunteer = models.BooleanField(default=False)
