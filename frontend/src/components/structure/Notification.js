92fb205c544a3090241eeee247a6cd7652b237c4
sktrip <sktripurari@gmail.com>
2021-01-25 21:41:50 +0000

    added error when changing email from profile page

---
+++ frontend/src/components/structure/Notification.js
@@ -0,0 +1,41 @@
+import { Snackbar, makeStyles } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        top: theme.spacing(15),
+    },
+}));
+
+function Notification(props) {
+    const classes = useStyles();
+    const { notify, setNotify, verticalPosTop } = props;
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setNotify({
+            ...notify,
+            isOpen: false,
+        });
+    };
+    return (
+        <Snackbar
+            className={classes.root}
+            open={notify.isOpen}
+            autoHideDuration={2000}
+            anchorOrigin={{
+                vertical: verticalPosTop ? "top" : "bottom",
+                horizontal: "center",
+            }}
+            onClose={handleClose}
+        >
+            <Alert severity={notify.type} onClose={handleClose}>
+                {notify.message}
+            </Alert>
+        </Snackbar>
+    );
+}
+
+export default Notification;
