dfd381254faa7fdb119556bb7f30e3c67c7712dd
nathalier <nathalier@gmail.com>
2021-01-31 01:23:22 +0000

    add int id to task_type - fixes bug with admin inability changing tasks

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -3,15 +3,22 @@ from .models import Task, TaskType
 from accounts.serializers import AccountSerializer
 
 
-class TaskSerializer(serializers.ModelSerializer):
-    requestee_details = AccountSerializer(source='requestee', read_only=True) 
-    volunteer_details = AccountSerializer(source='volunteer', read_only=True)
-    requested_vol_details = AccountSerializer(source='requested_volunteer', read_only=True)
-    task_type_name = serializers.CharField(source='get_task_type_display', read_only=True)
-    status_name = serializers.CharField(source='get_status_display', read_only=True)
+class TaskTypeSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = TaskType
+		fields = '__all__'
 
-    class Meta:
-        model = Task
-        fields = ("id", "task_type", "description", "dbs_required", "start_time",
-                  "end_time", "status", "requestee", "volunteer","requested_vol", "requestee_details",
-                  "volunteer_details", "task_type_name", "status_name", "requested_vol_details")
+
+class TaskSerializer(serializers.ModelSerializer):
+	requestee_details = AccountSerializer(source='requestee', read_only=True)
+	volunteer_details = AccountSerializer(source='volunteer', read_only=True)
+	requested_vol_details = AccountSerializer(source='requested_volunteer', read_only=True)
+	task_type_details = TaskTypeSerializer(source='task_type', read_only=True)
+	status_name = serializers.CharField(source='get_status_display', read_only=True)
+
+	class Meta:
+		model = Task
+		fields = ("id", "task_type", "task_type_details", "description", "dbs_required",
+		          "start_time", "end_time", "status", "status_name", "requestee", "volunteer",
+		          "requested_vol", "requestee_details", "volunteer_details",
+		          "requested_vol_details")
