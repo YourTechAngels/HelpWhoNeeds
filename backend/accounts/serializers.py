f117610897e689197f3e7eba7481bfab0fb0c5a7
nathalier <nathalier@gmail.com>
2021-02-06 14:20:11 +0000

    add search of close volunteers for requesting one by a requestee to accounts app

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,10 +3,18 @@ from .models import User
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    distance = serializers.SerializerMethodField("get_distance_vol_vul")
+
+    def get_distance_vol_vul(self, obj):
+        try:
+            return obj.dist
+        except AttributeError:
+            return None
+
     class Meta:
         model = User
-
         fields = ("id", "uid",  "email", "first_name", "last_name", "date_of_birth",
                   "phone_number", "post_code", "address_line_1", "address_line_2",
-                  "city", "county", "is_volunteer", "dbs", "latitude", "longitude", "location", "is_available")
+                  "city", "county", "is_volunteer", "dbs", "latitude", "longitude",
+                  "location", "is_available", "distance")
         depth = 1
